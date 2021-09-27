b1e2ac82d3176c14761c645da548cf49ff1a99db
Federico Ortiz <federico.aortiz99@gmail.com>
2021-09-27 13:29:42 -0300

    TP N°2

---
+++ TP N°2 Taller/Entidades/Ciclomotor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ciclomotor : Vehiculo
+    {
+        #region "Constructores"
+        public Ciclomotor(EMarca marca, string chasis, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+        }
+        #endregion
+
+        #region "Propiedades"
+        /// <summary>
+        /// Ciclomotor son 'Chico'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get { return ETamanio.Chico; }
+        }
+        #endregion
+
+        #region "Métodos"
+        /// <summary>
+        /// Publica todos los datos del Ciclomotor
+        /// </summary>
+        /// <returns></returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CICLOMOTOR");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("TAMAÑO : " +  this.Tamanio);
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
